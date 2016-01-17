f9b613084ed7fcbec034ac9e84619fa9d00a303d
Kahlil <kahlil29@gmail.com>
2016-01-17 15:48:20 +0530

    Revert "Restructure and revamp"
    This reverts commit c3a46a4aa0f82533849b56f4c26d95be8e17ad7e.

--- main.rb
+++ main.rb
@@ -16,23 +16,17 @@ item2 = GoanItem.new
 item2.name = "Fish Thali"
 item2.cost = 100
 
-
-
-
 item3 = GoanItem.new
 item3.name = "Cashewnuts"
 item3.cost = 20
-
 
 item4 = GoanItem.new
 item4.name = "Feni"
 item4.cost = 200
 
-
 item5 = GoanItem.new
 item5.name = "Port Wine"
 item5.cost = "250"
-
 
 item6 = GoanItem.new
 item6.name = "EDM Festival Pass"
@@ -46,31 +40,9 @@ item5.save
 item6.save
 
 
-before do
-  @item1name = item1.name
-  @item1cost = item1.cost
-  @item2name = item2.name
-  @item2cost = item2.cost
-  @item3name = item3.name
-  @item3cost = item3.cost
-  @item4name = item4.name
-  @item4cost = item4.cost
-  @item5name = item5.name
-  @item5cost = item5.cost
-  @item6name = item6.name
-  @item6cost = item6.cost
-
-end
-
-
 
 get '/' do
   erb :landing
-end
-
-post '/display' do
-  @selected_cost = params[:selected_cost]
-  erb :display
 end
 
 get '/compare' do
