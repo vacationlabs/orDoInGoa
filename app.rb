137cae3073879bc04d14766529675dbc193c9cf3
SharathSreekumar <sharathsreekumar10@gmail.com>
2016-01-17 17:40:39 +0530

    validate commit

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ get '/' do
 	@image = "http://img1a.flixcart.com/img/thumb-default.jpg"
 	@productName = nil
 	@price = nil
-	erb :index
+	erb :fixedHeaders
+end
+
+get '/alg' do
+	
 end
 
 post '/' do
@@ -22,17 +26,32 @@ post '/' do
 	url = "http://www.flipkart.com/search?q=" + product
 	doc = Nokogiri::HTML(open(url))
 	
-	@link = "http://www.flipkart.com" + doc.at_css(".unit-4 .pu-visual-section a")["href"]
-	#@image = doc.at_css(".unit-4 .pu-visual-section .fk-product-thumb img")["data-src"]
-	@productName = doc.at_css(".unit-4 .pu-details .pu-title").text.strip
-	puts doc.at_css(".unit-4 .pu-details .pu-title").text.strip
-	@price = doc.at_css(".unit-4 .pu-details .pu-price .pu-final").text.strip
+	if(doc.at_css(".unit-4") != nil)
+		#puts "unit 4"
 
-	url = @link
-	doc = Nokogiri::HTML(open(url))
-	@image = doc.at_css(".top-section .left-col-wrap .left-col .productImages .innerPanel .mainImage .imgWrapper img")["data-src"]
+		@link = "http://www.flipkart.com" + doc.at_css(".unit-4 .pu-visual-section a")["href"]
+		#@image = doc.at_css(".unit-4 .pu-visual-section .fk-product-thumb img")["data-src"]
+		@productName = doc.at_css(".unit-4 .pu-details .pu-title").text.strip
+		puts doc.at_css(".unit-4 .pu-details .pu-title").text.strip
+		@price = doc.at_css(".unit-4 .pu-details .pu-price .pu-final").text.strip
+		url = @link
+		doc = Nokogiri::HTML(open(url))
+		@image = doc.at_css(".top-section .left-col-wrap .left-col .productImages .innerPanel .mainImage .imgWrapper img")["data-src"]
+	elsif(doc.at_css(".unit-4") == nil && doc.at_css(".unit-3") != nil)
+		#puts "unit 3"
+
+		@link = "http://www.flipkart.com" + doc.at_css(".unit-3 .pu-visual-section a")["href"]
+		#@image = doc.at_css(".unit-4 .pu-visual-section .fk-product-thumb img")["data-src"]
+		@productName = doc.at_css(".unit-3 .pu-details .pu-title").text.strip
+		puts doc.at_css(".unit-3 .pu-details .pu-title").text.strip
+		@price = doc.at_css(".unit-3 .pu-details .pu-price .pu-final").text.strip
+		url = @link
+		doc = Nokogiri::HTML(open(url))
+		@image = doc.at_css(".top-section .left-col-wrap .left-col .productImages .innerPanel .mainImage .imgWrapper img")["data-src"]
+	else
+		puts "no units"
+	end
 
 	#redirect to("/")
-	erb :index
+	erb :fixedHeaders1
 end
-
--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,17 @@
-function hide(elem, search){
-	if $(elem).hasClass('active') {
-		$(search).show(2000);
-	else
-		$(search).hide(2000);
-	}
+function hide_stuff(l1,l2,r1,r2){
+	$.fn.multiscroll.moveSectionDown();
+	// $(l1).removeClass('active');
+	// $(r1).removeClass('active');
+	// $(l2).addClass('active');
+	// $(r2).addClass('active');
+	console.log("asdas");
 }
 
-$(document).ready(function() {
-	if $(document.body).hasClass('ms-viewing-first') {
-		//$('#inputGrp').show(2000);
-		alert($(document.body).hasClass('ms-viewing-first'));
-	}else {
-		$('#inputGrp').hide(2000);
-	}
-});
+// $(document).ready(function() {
+// 	if $(document.body).hasClass('ms-viewing-first') {
+// 		//$('#inputGrp').show(2000);
+// 		alert($(document.body).hasClass('ms-viewing-first'));
+// 	}else {
+// 		$('#inputGrp').hide(2000);
+// 	}
+// });
